bb5af22ffd41083bf945dd4c74de1b600af70682
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-07-25 00:14:29 +0900

    Re-solved abc134 d-prob

--- abc134/d/main.py
+++ abc134/d/main.py
@@ -1,10 +1,17 @@
+n = int(input())
+a = list(map(int, input().split()))
 
-N = int(input())
-a = [0]
-for i in range(N):
-    a.append(int(input()))
+b = [0] * n
+for i in range(n)[::-1]:
+    multiple = b[i::i + 1]
+    if sum(multiple) % 2 != a[i]:  # 偶奇が一致していない場合
+        b[i] = 1  # ボールを入れる
+    
+print(sum(b))
+true_b = []  # ボールが入っている箱の番号 true_b
+for i in range(n):
+    if b[i] == 0: continue
+    true_b.append(str(i + 1))
 
-count = 0
-for i in range(1, N + 1):
-    a[i] ==
-
+if len(true_b) != 0:
+    print(' '.join(true_b))
