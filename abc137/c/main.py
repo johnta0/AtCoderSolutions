4c1269dda5a0fa434d2e6d1d8ff608f3905eee24
johnta0 <22534726+johnta0@users.noreply.github.com>
2019-08-11 00:12:41 +0900

    解説聴いた後解き直した

--- abc137/c/main.py
+++ abc137/c/main.py
@@ -1,35 +1,19 @@
 import itertools
 
-def comb(n, r):
-    return len(list(itertools.combinations(range(n), r)))
-
-def get_runlength(s): # ex) s = 'RRLLLLRLRLRLRR'
-    ret = []
-    count = 1
-    for i in range(len(s)):
-        if i == len(s) - 1:
-            ret.append((s[i], count))
-            continue
-        if s[i] == s[i + 1]:
-            count += 1
-        else:
-            ret.append((s[i], count))
-            count = 1
-    return ret
-
 n = int(input())
-s = []
+dic = {}
 for i in range(n):
     sin = input()
     sin = list(sin)
     sin.sort()
     sin = ''.join(sin)
-    s.append(sin)
+    try:
+        dic[sin] += 1
+    except:
+        dic[sin] = 1
 
-ss = set(s)
 count = 0
-for u in ss:
-    al = s.count(u)
-    # s.remove
-    count += comb(al, 2)
+for v in dic.values():
+    count += v * (v - 1) / 2
+count = int(count)
 print(count)
